20bd87061f049ee570126af1c47330ddb27c44fc
Carlos_Mina <cmina@nexura.com>
2019-01-11 14:17:08 -0500

    sdff

--- index.js
+++ index.js
@@ -163,7 +163,9 @@ socketio.on('connection', function (socket) {
         if (text.trim() == 1 || text.trim() == 'Volver al menu') {
           socket.emit('ai response', mensajeHola);
           estadoFlujo = "tipoDoc";
-        } else if (text.trim() == 2 || text.trim() == 'Nada'.toLocaleLowerCase()) {
+          console.log(estadoFlujo);
+          
+        } else if (text.trim() == 2 || text.trim() == 'Nada') {
           let adios = "Adios " + usuario + ", hasta la próxima."
           socket.emit('ai response', adios);
           estadoFlujo = "menu";
