c49bada7686bf68b71b1453d39ff7bc2417a914d
Carlos_Mina <cmina@nexura.com>
2019-01-14 10:08:04 -0500

    sdff

--- index.js
+++ index.js
@@ -113,7 +113,7 @@ socketio.on('connection', function (socket) {
             " - <b>(CA)</b> Cancelar";
           socket.emit('ai response', mensajeHola);
           estadoFlujo = "tipoDoc";
-          users.set(sesion, usuario);
+          users.set("sesion", sesion, "usuario", usuario);
           console.log(users.set("sesion", sesion, "usuario", usuario));
           
           console.log(estadoFlujo);
