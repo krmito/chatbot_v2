0877d269ac023d107ab77a6ee11f2eb92cefd68d
Carlos_Mina <cmina@nexura.com>
2019-01-15 09:19:26 -0500

    sdff

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ socketio.on('connection', function (socket) {
   console.log("Usuario conectados: " + CONNECTED_USERS);
 //  console.log(Object.keys(io.sockets.sockets));
 
-  console.log('a user connected', socket.nsp.server.eio.clients);
+  console.log('a user connected', JSON.parse(`${socket.nsp.server.eio.clients}`));
 
 
 
